4432747ec7e0fbb71a90bb2156e9687eb017e215
lymos <lymoslymos@163.com>
2016-10-03 21:14:03 +0800

    update

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+class HomeController extends Controller{
+
+	public function getIndex(){
+
+		echo '444';
+	}
+	
+	public function about(){
+		echo 'about';
+	}
+}
